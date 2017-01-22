fc90d9300cab8b41d241600edcaa52ef7a6c7edc
kraflab <kraflab@gmail.com>
2017-01-22 01:18:04 +0000

    Added short home test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get "/home"
     assert_response :success
+    assert_select "title", "Home | DSDA"
+    assert_select "div.panel"
+    assert_select "div.panel-heading", "Demo Categories & Types"
+    assert_select "td", "Exit as fast as possible on skill 4."
   end
 
 end
