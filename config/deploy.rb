dc76e08406993a54d26ae8252c9873d3bcd3314f
kraflab <kraflab@gmail.com>
2017-03-08 04:57:52 +0000

    Removed cap seed

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,11 +58,6 @@ namespace :deploy do
       end
     end
   end
-  
-  desc "reload the database with seed data"
-  task :seed do
-    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
-  end
 
   desc 'Initial Deploy'
   task :initial do
