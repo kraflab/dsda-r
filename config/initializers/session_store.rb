f9b03621abaab339af7e47b5b9a2f1a8c369d4d5
kraflab <kraflab@gmail.com>
2017-03-14 02:31:33 +0000

    Removed secure cookie toggle

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_dsda-r_session",
-                                                      secure: Rails.env.production?,
-                                                      domain: (Rails.env.production? ? ".dsdarchive.com" : :all)
+Rails.application.config.session_store :cookie_store, key: "_dsda-r_session"
