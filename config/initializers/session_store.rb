5b9aff8add84f6deec86e3ab8b096e2cc4df1a90
kraflab <kraflab@gmail.com>
2017-03-14 01:53:27 +0000

    Updated session configuration

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_dsda-r_session',
-                                                      secure: Rails.env.production?
+Rails.application.config.session_store :cookie_store, key: "_dsda-r_session#{Rails.env}",
+                                                      secure: Rails.env.production?,
+                                                      domain: :all
