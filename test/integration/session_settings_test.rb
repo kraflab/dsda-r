d064a6a25b561ee3d8d153f7376b99f1b664cbe1
kraflab <kraflab@gmail.com>
2017-03-18 20:49:02 +0000

    Updated settings test

--- test/integration/session_settings_test.rb
+++ test/integration/session_settings_test.rb
@@ -8,7 +8,8 @@ class SessionSettingsTest < ActionDispatch::IntegrationTest
   test "settings" do
     get settings_path
     assert_response :success
-    patch settings_path, params: { "cat:UV Speed" => "0" }
-    assert_equal cookies["category_filter"], '{"filter":["UV Speed"]}'
+    patch settings_path, params: { "cat:UV Speed" => "0", "hideTas" => "0" }
+    assert_equal JSON.parse(cookies["category_filter"])["filter"], ["UV Speed"]
+    assert_equal JSON.parse(cookies["category_filter"])["hideTas"], true
   end
 end
