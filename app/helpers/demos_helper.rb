693a37fab16a9418a9e38a9525a149cd48e5d16a
Ryan Krafnick <kraflab@gmail.com>
2017-04-30 18:38:21 -0400

    Cleaned up wad show view

--- app/helpers/demos_helper.rb
+++ app/helpers/demos_helper.rb
@@ -16,4 +16,39 @@ module DemosHelper
   def last_update
     Demo.reorder(:updated_at).last.updated_at
   end
+  
+  def demo_level_cell(demo, chunk)
+    if demo == chunk.first
+      content_tag :td, demo.level, class: 'no-stripe-panel',
+        rowspan: 3 * chunk.count
+    end
+  end
+  
+  def demo_category_cell(demo, chunk, wad)
+    if demo == chunk.first
+      content_tag :td, class: 'no-stripe-panel', rowspan: "#{3 * chunk.count}" do
+        [
+          demo.category.name,
+          if (chunk.count > 5 and !chunk.any? { |i| i.recorded_at.nil? })
+            link_to record_timeline_path(id: wad.username, level: demo.level, category: demo.category.name) do
+              content_tag :span, '', class: 'glyphicon glyphicon-stats',
+                'aria-hidden': 'true', 'aria-label': 'Timeline'
+            end
+          else
+            nil
+          end
+        ].join(' ').html_safe
+      end
+    end
+  end
+  
+  def demo_tags(demo)
+    content_tag :tr, '' do
+      if demo.has_shown_tag
+        content_tag :td, demo.shown_tags_text, class: 'tag-text', colspan: '4'
+      else
+        content_tag :td, '', class: 'no-display', colspan: '4'
+      end
+    end
+  end
 end
--- app/helpers/wads_helper.rb
+++ app/helpers/wads_helper.rb
@@ -1,2 +1,37 @@
 module WadsHelper
+  
+  def wad_header(wad)
+    content_tag :h1 do
+      [
+        wad.name,
+        content_tag(:small, wad.author),
+        edit_wad_link(wad)
+      ].join(' ').html_safe
+    end
+  end
+  
+  def wad_sub_header(wad)
+    content_tag :p, class: 'p-short' do
+      [
+        demo_details(wad),
+        new_demo_link(wad)
+      ].join(' ').html_safe
+    end
+  end
+  
+  def edit_wad_link(wad)
+    if logged_in?
+      link_to edit_wad_path(wad), :class => 'btn btn-info btn-xs' do
+        content_tag :span, '', class: 'glyphicon glyphicon-cog', 
+          'aria-hidden': 'true', 'aria-label': 'Edit'
+      end
+    end
+  end
+  
+  def new_demo_link(wad)
+    if logged_in?
+      link_to 'Create New Demo', new_demo_path(wad: wad.username),
+        :class => 'label label-info'
+    end
+  end
 end
