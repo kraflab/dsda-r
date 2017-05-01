86ec3c62352ed5473169c6b76b1ca4f5c5bfb606
Ryan Krafnick <kraflab@gmail.com>
2017-04-30 22:22:11 -0400

    Cleaned up iwad index view

--- app/helpers/iwads_helper.rb
+++ app/helpers/iwads_helper.rb
@@ -1,5 +1,22 @@
 module IwadsHelper
   
+  def iwads_header(iwads)
+    [
+      content_tag(:h1, 'Iwad List'),
+      (content_tag :p, class: 'p-short' do
+        [
+          pluralize(iwads.count, 'iwad'),
+          if logged_in?
+            link_to 'Create New Iwad', new_iwad_path,
+              class: 'label label-info'
+          else
+            nil
+          end
+        ].join(' ').html_safe
+      end)
+    ].join(' ').html_safe
+  end
+  
   def iwad_by_wad_count
     Hash[Iwad.all.map {|i| [i.name, i.wads.count]}]
   end
