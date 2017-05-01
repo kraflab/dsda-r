d406aa9c6cce8307b8479fe3cc44c41daa1e6aba
Ryan Krafnick <kraflab@gmail.com>
2017-04-30 22:46:55 -0400

    Cleaned up port index view

--- app/helpers/ports_helper.rb
+++ app/helpers/ports_helper.rb
@@ -1,2 +1,15 @@
 module PortsHelper
+  
+  def ports_header(ports)
+    [
+      content_tag(:h1, 'Port List'),
+      if logged_in?
+        content_tag :p, class: 'p-short' do
+          link_to 'Create New Port', new_port_path, class: 'label label-info'
+        end
+      else
+        nil
+      end
+    ].join(' ').html_safe
+  end
 end
