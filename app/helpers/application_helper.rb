eb1dba09363512c40d040d7067b90c9240a14539
Ryan Krafnick <kraflab@gmail.com>
2017-04-30 22:37:22 -0400

    Cleaned up flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,20 @@ module ApplicationHelper
       "#{page_title} | #{base_title}"
     end
   end
+  
+  def flash_messages(flash)
+    flash.collect do |message_type, message|
+      sub_message = message.split('%%')
+      content_tag :div, class: "alert alert-#{message_type} alert-modest left-text" do
+        if sub_message.count == 1
+          message
+        else
+          [
+            content_tag(:strong, sub_message[0]),
+            sub_message[1]
+          ].join(' ').html_safe
+        end
+      end
+    end.join(' ').html_safe
+  end
 end
