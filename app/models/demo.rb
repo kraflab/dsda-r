7163039a6ae3ef4a8986cb0a4b67c478966d77ef
Ryan Krafnick <kraflab@gmail.com>
2017-05-20 16:40:15 -0400

    Added is_record? method for demos

--- app/models/demo.rb
+++ app/models/demo.rb
@@ -28,30 +28,30 @@ class Demo < ApplicationRecord
   def wad_username
     wad.username if wad
   end
-  
+
   def wad_username=(name)
     self.wad = Wad.find_by(username: name) unless name.blank?
     if wad.nil?
       errors.add(:wad_username, :not_found, message: "not found")
     end
   end
-  
+
   def category_name
     category.name if category
   end
-  
+
   def category_name=(name)
     self.category = Category.find_by(name: name) unless name.blank?
   end
-  
+
   def file_path
     demo_file.data.url if demo_file
   end
-  
+
   def time
     Demo.tics_to_string(tics, has_tics) if tics
   end
-  
+
   # [hh:]mm:ss[.tt]
   def time=(str)
     return if str.blank?
@@ -63,37 +63,52 @@ class Demo < ApplicationRecord
                 ( spl.count == 2 ? spl[1].to_i : 0 )
     self.has_tics = (spl.count == 2)
   end
-  
+
   def note
     "#{coop_text} #{tas_text}"
   end
-  
+
   def coop_text
     guys > 1 ? "#{guys}P" : ''
   end
-  
+
   def tas_text
     tas != 0 ? (tas > 0 ? "T#{tas}" : 'T') : ''
   end
-  
+
   def update_tags
     self.has_hidden_tag = sub_categories.where('style & ? = 0', SubCategory.Show).exists?
     self.has_shown_tag = sub_categories.where('style & ? > 0', SubCategory.Show).exists?
     self.save
   end
-  
+
   def hidden_tags_text
     cell_names(sub_categories.where('style & ? = 0', SubCategory.Show))
   end
-  
+
   def shown_tags_text
     cell_names(sub_categories.where('style & ? > 0', SubCategory.Show))
   end
-  
+
   def players_text
     cell_names(players)
   end
-  
+
+  # Return true if fastest demo (including ties) *to the second*
+  def is_record?
+    record_tics = Demo.where(wad: wad, level: level, category: category,
+                             tas: tas, guys: guys).minimum(:tics)
+    # Pacifist runs are also UV Speed runs
+    if category.name == 'UV Speed'
+      alt_record_tics = Demo.where(wad: wad, level: level,
+                                   category: Category.find_by(name: 'Pacifist'),
+                                   tas: tas, guys: guys).minimum(:tics)
+      record_tics = [record_tics, alt_record_tics].min if alt_record_tics
+    end
+
+    tics / 100 <= record_tics / 100
+  end
+
   def self.tics_to_string(t, with_tics = true)
     s = t / 100
     t %= 100
@@ -105,9 +120,9 @@ class Demo < ApplicationRecord
       ":#{s.to_s.rjust(2, '0')}" +
       (with_tics ? '.' + t.to_s.rjust(2, '0') : '')
   end
-  
+
   private
-  
+
     # delete related file if this is the only associated demo
     def check_file
       if demo_file and demo_file.demos.count == 1
@@ -119,7 +134,7 @@ class Demo < ApplicationRecord
     def update_players
       players.each { |i| i.touch }
     end
-    
+
     # collect names for table cell
     def cell_names(thing)
       thing.collect { |i| i.name }.join("\n")
--- test/models/demo_test.rb
+++ test/models/demo_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class DemoTest < ActiveSupport::TestCase
-  
+
   def setup
     @wad = wads(:btsx)
     @category = categories(:uvspeed)
@@ -10,90 +10,90 @@ class DemoTest < ActiveSupport::TestCase
                      has_tics: true, level: "Map 02",
                      recorded_at: 20.minutes.ago, levelstat: "0:00.99,1:11:00")
   end
-  
+
   test "should be valid" do
     assert @demo.valid?
   end
-  
+
   test "must have wad" do
     @demo.wad = nil
     assert_not @demo.valid?
   end
-  
+
   test "must have category" do
     @demo.category = nil
     assert_not @demo.valid?
   end
-  
+
   test "must have tics" do
     @demo.tics = nil
     assert_not @demo.valid?
   end
-  
+
   test "tics must be positive" do
     @demo.tics = 0
     assert_not @demo.valid?
     @demo.tics = -1
     assert_not @demo.valid?
   end
-  
+
   test "must have engine" do
     @demo.engine = ""
     assert_not @demo.valid?
   end
-  
+
   test "engine must not be too long" do
     @demo.engine = "a" * 51
     assert_not @demo.valid?
   end
-  
+
   test "must have tas" do
     @demo.tas = nil
     assert_not @demo.valid?
   end
-  
+
   test "tas must be positive" do
     @demo.tas = -1
     assert_not @demo.valid?
   end
-  
+
   test "must have guys" do
     @demo.guys = nil
     assert_not @demo.valid?
   end
-  
+
   test "guys must be positive" do
     @demo.guys = 0
     assert_not @demo.valid?
     @demo.guys = -1
     assert_not @demo.valid?
   end
-  
+
   test "must have level" do
     @demo.level = ""
     assert_not @demo.valid?
   end
-  
+
   test "level must not be too long" do
     @demo.level = "a" * 11
     assert_not @demo.valid?
   end
-  
+
   test "does not need recorded_at" do
     @demo.recorded_at = nil
     assert @demo.valid?
   end
-  
+
   test "must have levelstat" do
     @demo.levelstat = ""
     assert_not @demo.valid?
   end
-  
+
   test "levelstat must not be too long" do
     @demo.levelstat = "a" * 501
     assert_not @demo.valid?
   end
-  
+
   test "should show notes properly" do
     assert_match @demo.note.strip, ""
     @demo.guys = 2
@@ -103,8 +103,19 @@ class DemoTest < ActiveSupport::TestCase
     @demo.guys = 1
     assert_match @demo.note.strip, "T3"
   end
-  
+
   test "time text should be right" do
     assert_match @demo.time, "0:00.99"
+  end
+
+  test "should determine record properly" do
+    demo01 = demos(:bt01speed)
+    demo01p = demos(:bt01pacifist)
+    demo02 = demos(:bt02speed)
+    demo02_slow = demos(:bt02speed_slow)
+    assert demo01p.is_record?
+    assert_not demo01.is_record?
+    assert demo02.is_record?
+    assert_not demo02_slow.is_record?
   end
 end
