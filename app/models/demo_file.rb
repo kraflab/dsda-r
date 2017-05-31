2c79aa4cbb1faaf75d8159f511781658deb9de26
Ryan Krafnick <kraflab@gmail.com>
2017-05-31 17:08:17 -0400

    Updated file validations

--- app/models/demo_file.rb
+++ app/models/demo_file.rb
@@ -4,14 +4,15 @@ class DemoFile < ApplicationRecord
   validates :wad_id, presence: true
   validates :md5, presence: true, uniqueness: true
   mount_uploader :data, ZipFileUploader
+  validates_presence_of :data
   validates_size_of :data, maximum: 1.megabytes, message: 'File exceeds 1 MB size limit'
   before_validation :compute_md5_hash
-  
+
   private
-  
+
     # Determine md5 hash for uniqueness test
     def compute_md5_hash
-      self.md5 = if data.file.file
+      self.md5 = if data and data.file and data.file.file
         Digest::MD5.hexdigest(data.file.read)
       else
         nil
--- app/models/wad_file.rb
+++ app/models/wad_file.rb
@@ -4,6 +4,7 @@ class WadFile < ApplicationRecord
   validates :iwad_id, presence: true
   validates :md5, presence: true, uniqueness: true
   mount_uploader :data, ZipFileUploader
+  validates_presence_of :data
   validates_size_of :data, maximum: 100.megabytes, message: 'File exceeds 100 MB size limit'
   before_validation :compute_md5_hash
 
@@ -11,7 +12,7 @@ class WadFile < ApplicationRecord
 
     # Determine md5 hash for uniqueness test
     def compute_md5_hash
-      self.md5 = if data.file.file
+      self.md5 = if data and data.file and data.file.file
         Digest::MD5.hexdigest(data.file.read)
       else
         nil
