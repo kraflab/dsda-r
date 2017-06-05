232b9fe23518eacc80e4d055c911230d1ac6aa5f
Ryan Krafnick <kraflab@gmail.com>
2017-06-05 16:06:38 -0400

    Added wad file tests

---
+++ test/models/wad_file_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class WadFileTest < ActiveSupport::TestCase
+
+  def setup
+    @iwad = iwads(:doom2)
+    @file = File.open(Rails.root.join("test/fixtures/files/test.zip"))
+    @wad_file = WadFile.new(iwad: @iwad, data: @file)
+  end
+
+  test "should be valid" do
+    assert @wad_file.valid?
+  end
+
+  test "must have iwad" do
+    @wad_file.iwad = nil
+    assert_not @wad_file.valid?
+  end
+
+  test "must be unique" do
+    @wad_file.save
+    new_file = WadFile.new(iwad: @iwad, data: @file)
+    new_file.valid?
+    assert_not new_file.valid?
+  end
+end
