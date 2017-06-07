f8d995dd5a21c1d0e3e1ffb415bf3de20598d592
Ryan Krafnick <kraflab@gmail.com>
2017-06-07 00:17:24 -0400

    Added movie comparison ajax test

--- test/integration/compare_movies_test.rb
+++ test/integration/compare_movies_test.rb
@@ -2,21 +2,41 @@ require 'test_helper'
 
 class CompareMoviesTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @demo_a = demos(:btep1_a)
+    @demo_b = demos(:btep1_b)
+  end
+
   test "should compare levelstats" do
-    demo_a = demos(:btep1_a)
-    demo_b = demos(:btep1_b)
     get compare_movies_path, params: {
-      category: demo_a.category.name,
-      id: demo_a.wad.username,
-      level: demo_a.level
+      category: @demo_a.category.name,
+      id: @demo_a.wad.username,
+      level: @demo_a.level
     }
     assert_response :success
-    [demo_a, demo_b].each do |demo|
+    [@demo_a, @demo_b].each do |demo|
       demo.levelstat.split("\n").each do |time|
         assert_select "td", time
       end
     end
     assert_select "td", "5"
     assert_select "td", "-5"
+  end
+
+  test "should respond to ajax" do
+    get compare_movies_json_path, params: {
+      category: @demo_a.category.name,
+      id: @demo_a.wad.username,
+      level: @demo_a.level,
+      index_0: 0,
+      index_1: 1
+    }
+    assert_response :success
+    hash = JSON.parse(response.body)
+    assert_equal hash['error'], false
+    assert_not hash['times'].nil?
+    assert_equal hash['times'].size, 2
+    assert_equal hash['times'].first.size, 3
+    assert_equal hash['times'].first[2], 5
   end
 end
