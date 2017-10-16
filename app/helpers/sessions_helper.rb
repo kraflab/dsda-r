51073eb8567fe1fadabeadba30a016a91a90bb05
Ryan Krafnick <kraflab@gmail.com>
2017-10-16 21:44:27 +0200

    Remove assorted log in logic

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,17 +1,4 @@
 module SessionsHelper
-
-  # Returns the current logged-in admin (if any)
-  def current_admin
-    @current_admin ||= Admin.find_by(username: session[:username])
-  end
-
-  # Returns true if this is an admin session
-  # Admin functionality is currently suppressed, except where explicitly forced
-  def logged_in?(force_answer = false)
-    force_answer ? !current_admin.nil? : false
-  end
-
-  # Returns array of demo filters
   def demo_filter_array
     JSON.parse(cookies['demo_filter'])
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -29,25 +29,9 @@ end
 class ActiveSupport::TestCase
   fixtures :all
 
-  # Returns true if a test admin is logged in.
-  def is_logged_in?
-    !session[:username].nil?
-  end
-
   # Returns a dummy zip for tests
   def dummy_zip(n = 0)
     File.open(Rails.root.join("test/fixtures/files/test#{n}.zip"))
-  end
-
-  # Logs in a test admin
-  def log_in_as(admin, options = {})
-    password = options[:password] || 'password1234'
-    if integration_test?
-      post login_path, params: { session: { username: admin.username,
-                                            password: password } }
-    else
-      session[:username] = admin.username
-    end
   end
 
   # get total time for thing's demos
