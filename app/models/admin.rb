693cf737e2906c315d9db7fb37a33f3a6007ff15
Ryan Krafnick <kraflab@gmail.com>
2018-02-18 19:58:16 +0100

    Revise admin model and add spec

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,6 @@
 class Admin < ApplicationRecord
+  LOGIN_FAIL_LIMIT = 5
+
   validates :fail_count, numericality: { greater_than_or_equal_to: 0 }
   validates :username, presence: true, length: { maximum: 50 },
                        uniqueness: true,
@@ -6,10 +8,23 @@ class Admin < ApplicationRecord
   validates :password, length: { minimum: 12 }, allow_blank: true
   has_secure_password
 
-  # Returns the hash digest of the given string.
   def Admin.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def account_locked?
+    fail_count >= LOGIN_FAIL_LIMIT
+  end
+
+  def fail_login!
+    update(fail_count: fail_count + 1)
+  end
+
+  def try_authenticate(password)
+    return nil if account_locked?
+    return self if authenticate(password)
+    fail_login!
+    nil
   end
 end
