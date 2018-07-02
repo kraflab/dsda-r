eaa0e98325204b7796098c8fd61916f1db16a7e7
Ryan Krafnick <kraflab@gmail.com>
2018-07-02 11:22:00 +0200

    Remove unused callbacks in category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,19 +4,9 @@ class Category < ApplicationRecord
                           uniqueness: true
   validates :description, presence: true, length: { maximum: 200 }
   validates :game,        presence: true, length: { maximum: 50 }
-  before_save   :clean_strings
-  before_update :clean_strings
 
   # called rarely, and few categories
   def self.select_list
     Category.all.collect { |i| [i.name, i.name] }
   end
-
-  private
-
-    # Remove excess whitespace
-    def clean_strings
-      self.name.strip.gsub!(/\s+/, ' ')
-      self.description.strip.gsub!(/\s+/, ' ')
-    end
 end
