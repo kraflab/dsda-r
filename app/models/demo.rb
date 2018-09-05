62c8f52222e70ae0a7bf9981988e5a75318b99c9
Ryan Krafnick <kraflab@gmail.com>
2018-09-05 17:49:26 +0200

    Fix episode demo list

--- app/models/demo.rb
+++ app/models/demo.rb
@@ -13,7 +13,7 @@ class Demo < ApplicationRecord
   scope :show_movies, -> { where("level LIKE 'Ep%' OR level LIKE '%A%'") }
   scope :show_ils, -> { where("level NOT LIKE 'Ep%' AND level NOT LIKE '%A%'") }
   scope :episode, ->(ep) {
-    reorder(:level).where("level <> ? AND (level LIKE ? or level = ? or LEVEL LIKE ? or LEVEL = ? #{ep == 2 ? 'or LEVEL = \'Map 31\' or LEVEL = \'Map 32\'' : ''})",
+    reorder(:level, :category_id, :tics).where("level <> ? AND (level LIKE ? or level = ? or LEVEL LIKE ? or LEVEL = ? #{ep == 2 ? 'or LEVEL = \'Map 31\' or LEVEL = \'Map 32\'' : ''})",
           "Map #{ep - 1}0", "Map #{ep - 1}_", "Map #{ep}0", "E#{ep}M%", "Ep #{ep}")
   }
   scope :recent, ->(n) { reorder(recorded_at: :desc).limit(n) }
