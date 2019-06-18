387a18f23dc7da0cced0680a23ee23e4e55ece67
Ryan Krafnick <kraflab@gmail.com>
2019-06-18 19:38:48 +0200

    Increase wad size limit

--- app/models/wad_file.rb
+++ app/models/wad_file.rb
@@ -5,5 +5,5 @@ class WadFile < ApplicationRecord
   validates :md5, presence: true, uniqueness: true
   mount_uploader :data, ZipFileUploader
   validates_presence_of :data
-  validates_size_of :data, maximum: 100.megabytes, message: 'File exceeds 100 MB size limit'
+  validates_size_of :data, maximum: 200.megabytes, message: 'File exceeds 200 MB size limit'
 end
