9d08e71f600d37a94936b31ff55fce346cd9e362
Ryan Krafnick <kraflab@gmail.com>
2020-04-09 16:15:09 +0200

    Add jwt strategy to admin auth

--- app/services/admin_authenticator.rb
+++ app/services/admin_authenticator.rb
@@ -5,21 +5,39 @@ class AdminAuthenticator
 
   def authenticate!
     extract_credentials_from_request
-    admin = authenticate_via_password
     raise Errors::Unauthorized unless admin
     admin
   end
 
   private
 
+  def admin
+    @admin ||= @jwt ? authenticate_via_jwt : authenticate_via_password
+  end
+
+  # deprecated
   def authenticate_via_password
     admin = Admin.find_by(username: @username)
     return nil unless admin
     admin.try_authenticate(@password)
   end
 
+  def authenticate_via_jwt
+    Admin.find_by(id: id_from_jwt)
+  rescue JsonWebToken::DecodeError
+    nil
+  end
+
+  def id_from_jwt
+    JsonWebToken.decode(@jwt)[:sub].to_i
+  end
+
   def extract_credentials_from_request
+    # deprecated
     @username = @request.headers['HTTP_API_USERNAME']
     @password = @request.headers['HTTP_API_PASSWORD']
+
+    # future
+    @jwt = @request.headers['Authorization']&.split(' ')&.last
   end
 end
--- test/services/admin_authenticator_test.rb
+++ test/services/admin_authenticator_test.rb
@@ -4,39 +4,74 @@ describe AdminAuthenticator do
   let(:request) {
     OpenStruct.new(
       headers: {
-        'HTTP_API_USERNAME' => username,
-        'HTTP_API_PASSWORD' => password
+        'Authorization' => "Bearer #{token}"
       }
     )
   }
   let(:admin) { admins(:elim) }
-  let(:username) { admin.username }
-  let(:password) { 'password1234' }
+  let(:token) { JsonWebToken.encode(admin) }
   let(:service) { AdminAuthenticator.new(request) }
 
   describe '#authenticate!' do
     let(:authenticate) { service.authenticate! }
 
     describe 'when the admin does not exist' do
-      let(:username) { 'wrong' }
+      let(:admin) { Admin.new(id: 0) }
 
       it 'raises an error' do
         proc { authenticate }.must_raise Errors::Unauthorized
       end
     end
 
-    describe 'when the admin exists' do
+    describe 'when the token is bad' do
+      let(:token) { 'bad-token' }
+
+      it 'raises an error' do
+        proc { authenticate }.must_raise Errors::Unauthorized
+      end
+    end
+
+    describe 'when the token is correct and the admin exists' do
       describe 'and the password is correct' do
         it 'returns the admin' do
           authenticate.must_equal admin
         end
       end
+    end
 
-      describe 'but the password is wrong' do
-        let(:password) { 'wrong' }
+    describe 'deprecated authentication' do
+      let(:request) {
+        OpenStruct.new(
+          headers: {
+            'HTTP_API_USERNAME' => username,
+            'HTTP_API_PASSWORD' => password
+          }
+        )
+      }
+      let(:username) { admin.username }
+      let(:password) { 'password1234' }
+
+      describe 'when the admin does not exist' do
+        let(:username) { 'wrong' }
 
         it 'raises an error' do
           proc { authenticate }.must_raise Errors::Unauthorized
+        end
+      end
+
+      describe 'when the admin exists' do
+        describe 'and the password is correct' do
+          it 'returns the admin' do
+            authenticate.must_equal admin
+          end
+        end
+
+        describe 'but the password is wrong' do
+          let(:password) { 'wrong' }
+
+          it 'raises an error' do
+            proc { authenticate }.must_raise Errors::Unauthorized
+          end
         end
       end
     end
