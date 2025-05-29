131d2cb9babf55acf475e2ae8f93934597ad962d
Pedro Beirão <82064173+Pedro-Beirao@users.noreply.github.com>
2025-05-30 00:09:04 +0100

    Add allowed ports table (#73)

--- app/helpers/ports_helper.rb
+++ app/helpers/ports_helper.rb
@@ -1,6 +1,16 @@
 module PortsHelper
 
-  def ports_header(ports)
-    content_tag(:h1, 'Port List')
+  def ports_table(ports, allowed)
+    ports.group_by(&:family).map do |family, ports|
+      content_tag(:tr) do
+        content_tag(:td, family) +
+        content_tag(:td) do
+          ports.each_with_index.map do |port, index|
+            link = link_to(port.version, port.file_path)
+            index == ports.size - 1 ? link : (link + ', ')
+          end.join.html_safe
+        end
+      end
+    end.join.html_safe
   end
 end
